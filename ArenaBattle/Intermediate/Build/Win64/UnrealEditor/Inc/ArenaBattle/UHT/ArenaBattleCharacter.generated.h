c8d581bcd7114490568d95cec4384b949c58ed79
9yu22 <ehtlfk2@naver.com>
2024-01-22 12:48:49 +0900

    이득우의 언리얼 c++ 게임개발의 정석 (~p.71)

---
+++ ArenaBattle/Intermediate/Build/Win64/UnrealEditor/Inc/ArenaBattle/UHT/ArenaBattleCharacter.generated.h
@@ -0,0 +1,64 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "ArenaBattleCharacter.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ARENABATTLE_ArenaBattleCharacter_generated_h
+#error "ArenaBattleCharacter.generated.h already included, missing '#pragma once' in ArenaBattleCharacter.h"
+#endif
+#define ARENABATTLE_ArenaBattleCharacter_generated_h
+
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_SPARSE_DATA
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_ACCESSORS
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAArenaBattleCharacter(); \
+	friend struct Z_Construct_UClass_AArenaBattleCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AArenaBattleCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ArenaBattle"), NO_API) \
+	DECLARE_SERIALIZER(AArenaBattleCharacter)
+
+
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AArenaBattleCharacter(AArenaBattleCharacter&&); \
+	NO_API AArenaBattleCharacter(const AArenaBattleCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AArenaBattleCharacter); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AArenaBattleCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AArenaBattleCharacter) \
+	NO_API virtual ~AArenaBattleCharacter();
+
+
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_18_PROLOG
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_SPARSE_DATA \
+	FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_ACCESSORS \
+	FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_INCLASS_NO_PURE_DECLS \
+	FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ARENABATTLE_API UClass* StaticClass<class AArenaBattleCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ ArenaBattle/Intermediate/Build/Win64/UnrealGame/Inc/ArenaBattle/UHT/ArenaBattleGameMode.generated.h
@@ -0,0 +1,64 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "ArenaBattleGameMode.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ARENABATTLE_ArenaBattleGameMode_generated_h
+#error "ArenaBattleGameMode.generated.h already included, missing '#pragma once' in ArenaBattleGameMode.h"
+#endif
+#define ARENABATTLE_ArenaBattleGameMode_generated_h
+
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_SPARSE_DATA
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_ACCESSORS
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAArenaBattleGameMode(); \
+	friend struct Z_Construct_UClass_AArenaBattleGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AArenaBattleGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ArenaBattle"), ARENABATTLE_API) \
+	DECLARE_SERIALIZER(AArenaBattleGameMode)
+
+
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	ARENABATTLE_API AArenaBattleGameMode(AArenaBattleGameMode&&); \
+	ARENABATTLE_API AArenaBattleGameMode(const AArenaBattleGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(ARENABATTLE_API, AArenaBattleGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AArenaBattleGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AArenaBattleGameMode) \
+	ARENABATTLE_API virtual ~AArenaBattleGameMode();
+
+
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_9_PROLOG
+#define FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_SPARSE_DATA \
+	FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_ACCESSORS \
+	FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ARENABATTLE_API UClass* StaticClass<class AArenaBattleGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_practiceUE_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
